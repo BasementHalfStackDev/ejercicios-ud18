a00844e4f72f8449fad489c425cf6fd8cdd2bc30
Josep Maria Pallas Batalla <jpb_montblanc@hotmail.com>
2023-03-29 13:46:09 +0200

    Creadas tablas ej 1

--- UD18-TA18/src/ejercicio_1/Ejercicio_1_App.java
+++ UD18-TA18/src/ejercicio_1/Ejercicio_1_App.java
@@ -4,17 +4,22 @@ import java.sql.Connection;
 
 import java_sql_utils.Java_SQL;
 
-
 public class Ejercicio_1_App {
 
 	public static void main(String[] args) {
-		
+
 		Connection conexion = Java_SQL.conectarDB();
+
+		Java_SQL.createDB("UD18_EJ1", conexion);
+
+		Java_SQL.createTable(conexion, "UD18_EJ1", "fabricantes",
+				"(codigo INT PRIMARY KEY, nombre VARCHAR(100))");
+		Java_SQL.createTable(conexion, "UD18_EJ1", "articulos",
+				"(codigo INT PRIMARY KEY AUTO_INCREMENT, nombre VARCHAR(100) NOT NULL, precio INT NOT NULL, "
+				+ "fabricante INT NOT NULL, FOREIGN KEY (fabricante) REFERENCES fabricantes(codigo))");
 		
-		Java_SQL.createDB("UD18-EJ1", conexion);
 		
-		Java_SQL.createTable(conexion, null, null, null);
-		
+
 		Java_SQL.closeConnection(conexion);
 
 	}
