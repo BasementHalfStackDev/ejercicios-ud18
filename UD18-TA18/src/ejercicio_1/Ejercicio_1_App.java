09b52667c75c8bb6a083df267419e8cd82b178ce
Josep Maria Pallas Batalla <jpb_montblanc@hotmail.com>
2023-03-29 16:54:37 +0200

    not null agregado al nombre de fabricante

--- UD18-TA18/src/ejercicio_1/Ejercicio_1_App.java
+++ UD18-TA18/src/ejercicio_1/Ejercicio_1_App.java
@@ -15,7 +15,8 @@ public class Ejercicio_1_App {
 		Java_SQL.createDB("UD18_EJ1", conexion);
 
 		// Create Tables
-		Java_SQL.createTable(conexion, "UD18_EJ1", "fabricantes", "(codigo INT PRIMARY KEY, nombre VARCHAR(100))");
+		Java_SQL.createTable(conexion, "UD18_EJ1", "fabricantes",
+				"(codigo INT PRIMARY KEY, nombre VARCHAR(100) NOT NULL)");
 		Java_SQL.createTable(conexion, "UD18_EJ1", "articulos",
 				"(codigo INT PRIMARY KEY AUTO_INCREMENT, nombre VARCHAR(100) NOT NULL, precio INT NOT NULL, "
 						+ "fabricante INT NOT NULL, FOREIGN KEY (fabricante) REFERENCES fabricantes(codigo))");
